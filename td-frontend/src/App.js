38d9d5cec79cd0d4dae85c33cd6ea65f470c91ae
Alejo Ceballos <alejo.ceballos@thoughtworks.com>
2019-06-18 13:25:19 -0300

    Add create and update hooks for study purposes

--- td-frontend/src/App.js
+++ td-frontend/src/App.js
@@ -2,12 +2,59 @@ import React from 'react';
 import './App.css';
 import CharacterSheet from "./components/character/character-sheet/character-sheet";
 
-function App() {
-  return (
-    <div className="App">
-      <CharacterSheet />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+
+  };
+
+  constructor(props) {
+    super(props);
+    console.log('C - [App] constructor');
+  }
+
+  static getDerivedStateFromProps(
+    props, state
+  ) {
+    console.log(
+      'C,U - [App] getDerivedStateFromProps', {props, state, }
+    );
+
+    return true;
+  }
+
+  shouldComponentUpdate(
+    nextProps, nextState
+  ) {
+    console.log(
+      'U - [App] shouldComponentUpdate', {nextProps, nextState, }
+    );
+  }
+
+  render() {
+    console.log('C - [App] render');
+    return (
+      <div className="App">
+        <CharacterSheet />
+      </div>
+    );
+  }
+
+  getSnapshotBeforeUpdate(
+    prevProps, prevState
+  ) {
+    console.log(
+      'U - [App] getSnapshotBeforeUpdate', {prevProps, prevState, }
+    );
+  }
+
+  componentDidMount() {
+    console.log('C - [App] componentDidMount');
+  }
+
+  componentDidUpdate() {
+    console.log('U - [App] componentDidUpdate');
+  }
+
 }
 
 export default App;
--- td-frontend/src/components/character/character-sheet/character-sheet.js
+++ td-frontend/src/components/character/character-sheet/character-sheet.js
@@ -5,6 +5,59 @@ import AttributesPanel from "./attributes-panel";
 
 const Container = styled(BorderedStyle)``;
 
-const CharacterSheet = () => <Container><AttributesPanel data-qa="attributesPanel"/></Container>;
+class CharacterSheet extends React.Component {
+  state = {
+
+  };
+
+  constructor(props) {
+    super(props);
+    console.log('C - [CharacterSheet] constructor');
+  }
+
+  static getDerivedStateFromProps(
+    props, state
+  ) {
+    console.log(
+      'C,U - [CharacterSheet] getDerivedStateFromProps', {props, state, }
+    );
+
+    return true;
+  }
+
+  shouldComponentUpdate(
+    nextProps, nextState
+  ) {
+    console.log(
+      'U - [CharacterSheet] shouldComponentUpdate', {nextProps, nextState, }
+    );
+  }
+
+  render() {
+    console.log('C - [CharacterSheet] render');
+    return (
+      <Container>
+        <AttributesPanel data-qa="attributesPanel"/>
+      </Container>
+    );
+  }
+
+  getSnapshotBeforeUpdate(
+    prevProps, prevState
+  ) {
+    console.log(
+      'U - [CharacterSheet] getSnapshotBeforeUpdate', {prevProps, prevState, }
+    );
+  }
+
+  componentDidMount() {
+    console.log('C - [CharacterSheet] componentDidMount');
+  }
+
+  componentDidUpdate() {
+    console.log('U - [CharacterSheet] componentDidUpdate');
+  }
+
+}
 
 export default CharacterSheet;
